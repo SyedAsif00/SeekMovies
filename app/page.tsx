759619715daefe7ac5de83962ab49ce3dd4b35ce
syedasif00 <syedasifullah00@gmail.com>
2025-01-31 12:19:00 -0500

    global styles folder created

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { redirect } from "next/navigation";
 import Approutes from "./_lib/AppRoutes";
+import "@/app/_styles/global.css";
 export default function Home() {
   redirect(Approutes.Movies);
 }
